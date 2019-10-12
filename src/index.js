36bde2a9e4c6e92fd5f2f956104e0d7f79b1ee72
danielospina@gmail.com <danielospina@gmail.com>
2019-10-11 19:52:11 -0500

    rutas

--- src/index.js
+++ src/index.js
@@ -5,9 +5,19 @@ import {App, Page1, Page2, NotFound} from './App';
 import './index.css';
 
 
+const routes = (
+  <Router history={hashHistory}>
+    <Route path='/' component={App}>
+      <IndexRedirect to='/page1'/>
+      <Route path='/page1' component={Page1}/>
+      <Route path='/page2' component={Page2}/>
+      <Route path='*' component={NotFound}/>
+    </Route>
+  </Router>
+)
 // Implementa las rutas aca
 
 ReactDOM.render(
-  <App />, // Es posible que tengas que hacer un cambio en esta linea.
+  routes, // Es posible que tengas que hacer un cambio en esta linea.
   document.getElementById('root')
 );
